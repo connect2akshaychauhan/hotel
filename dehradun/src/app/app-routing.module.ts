1b1db45b4c63fa98a3f40abfa211958c9b039295
Akshay Chauhan <connect2akshaychauhan@gmail.com>
2020-06-19 09:46:05 +0530

    routing changes

--- dehradun/src/app/app-routing.module.ts
+++ dehradun/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+import { EmployeeComponent } from './employee/employee.component';
+const routes: Routes = [
+  { path: 'home', component:  HomeComponent },
+  { path: 'contactus', component:  ContactusComponent },
+  { path: 'about', component:  AboutComponent },
+  { path: 'employee', component:  EmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dehradun/src/app/app.module.ts
+++ dehradun/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HttpClientModule } from '@angular/common/http';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    ContactusComponent,
+    AboutComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
